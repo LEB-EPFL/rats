0f3b7badc0cf85a1ea4e616c7bd6f2f3ed12182c
Kyle Douglass <kyle.douglass@epfl.ch>
2022-11-18 15:39:29 +0100

    Simple implementation of StateMachine::step

--- src/lib.rs
+++ src/lib.rs
@@ -5,24 +5,32 @@
 use rand::prelude::*;
 
 type State = u32;
+type Time = f64;
 
 struct Event {
     from: State,
-    time: f64,
+    time: Time,
     to: State,
 }
 
 struct StateMachine {
     current_state: State,
+    num_states: u32,
 }
 
 impl StateMachine {
-    fn new(current_state: State) -> Self {
-        StateMachine { current_state }
+    fn new(current_state: State, num_states: u32) -> Self {
+        StateMachine { current_state, num_states }
     }
 
     fn step<R: rand::Rng + ?Sized>(&mut self, rng: &mut R) {
-        let new_state: u32 = rng.gen_range(0..10);
+        let mut new_state: u32;
+        loop {
+            new_state = rng.gen_range(0..self.num_states - 1);
+            if new_state != self.current_state {
+                break
+            }
+        }
         self.current_state = new_state;
     }
 }
--- src/tests.rs
+++ src/tests.rs
@@ -1,8 +1,21 @@
 #[cfg(test)]
+use rand::prelude::*;
+
 use crate::StateMachine;
 
 #[test]
 fn new_state_machine() {
-    let result = StateMachine::new(0);
+    let result = StateMachine::new(0, 10);
     assert_eq!(result.current_state, 0);
+}
+
+#[test]
+fn step_state_machine() {
+    let mut rng = rand::thread_rng();
+    let mut sm = StateMachine::new(0, 10);
+    let old_state = sm.current_state;
+
+    sm.step(&mut rng);
+
+    assert_ne!(old_state, sm.current_state);
 }
