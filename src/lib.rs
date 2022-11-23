0eb650e3d8e4bd69dd26aad9d5e5e4806cfc830a
Kyle Douglass <kyle.douglass@epfl.ch>
2022-11-23 12:01:17 +0100

    Add Accumulator to collect events

--- src/lib.rs
+++ src/lib.rs
@@ -4,19 +4,23 @@
 ///
 use rand::prelude::*;
 
+type State = u32;
+type Time = f64;
+
 struct Event {
-    from: u32,
-    time: f64,
-    to: u32,
+    from: State,
+    time: Time,
+    to: State,
 }
 
+/// A memoryless, probabilistic state machine.
 struct StateMachine {
-    current_state: u32,
-    num_states: u32,
+    current_state: State,
+    num_states: State,
 }
 
 impl StateMachine {
-    fn new(current_state: u32, num_states: u32) -> Self {
+    fn new(current_state: State, num_states: State) -> Self {
         StateMachine {
             current_state,
             num_states,
@@ -25,7 +29,7 @@ impl StateMachine {
 
     fn step<R: rand::Rng + ?Sized>(&mut self, rng: &mut R) -> Event {
         let old_state = self.current_state;
-        let mut new_state: u32;
+        let mut new_state: State;
         loop {
             new_state = rng.gen_range(0..self.num_states - 1);
             if new_state != self.current_state {
@@ -40,6 +44,47 @@ impl StateMachine {
             time: 0.25,
             to: new_state,
         }
+    }
+}
+
+struct Accumulator {
+    state_machine: StateMachine,
+    t_cutoff: Time,
+    event_buffer: Vec<Event>,
+}
+
+impl Accumulator {
+    fn new(state_machine: StateMachine) -> Self {
+        let event_buffer = Vec::new();
+
+        Accumulator {
+            state_machine,
+            t_cutoff: 1.0,
+            event_buffer,
+        }
+    }
+
+    /// Steps a state machine until the cumulative sum of event times exceeds a given limit.
+    fn accumulate<R: rand::Rng + ?Sized>(&mut self, rng: &mut R) -> &mut [Event] {
+        self.event_buffer.clear();
+
+        let mut t_cumulative: Time = 0.0;
+        let mut event: Event;
+        loop {
+            event = self.state_machine.step(rng);
+
+            event.time += t_cumulative;
+            if event.time > self.t_cutoff {
+                // The state machine is assumed memoryless, so we don't need to save the event for
+                // future calls to this function.
+                break;
+            } else {
+                t_cumulative = event.time;
+                self.event_buffer.push(event);
+            }
+        }
+
+        self.event_buffer.as_mut_slice()
     }
 }
 
